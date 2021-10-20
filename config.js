2cf42b7d945e333253d90f18fd0122e1f12030af
jcognnei <92803030+jcognnei@users.noreply.github.com>
2021-10-20 09:33:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,12 @@ const CONFIG = {
     title: 'Đã được nhân quà 20/10 chưa ấy nhỉ 😙',
     btnYes: 'Gòi :3',
     btnNo: 'Đến bây giờ vẫn chưa',
-    question:'Sao Girls Xóm Nghiện lại tế thế có ai biết tại sao không',
+    question:'Sao Girls Xóm Nghiện lại xinh tế, thế có ai biết tại sao không',
     btnReply: 'Gửi thông điệp tới vũ trụ',
     reply: 'Xinh là do nhậu nhiều ',
     mess: 'Hép pi 20/10 he he 🥰',
-    messDesc: 'Hẹn các bạn Xinh gái trên bàn nhậu lúc đi học nhá',
+    messDesc: 'Hẹn các bạn Xinh gái trên bàn nhậu lúc đi học nhá
+    Chúc các bạn nữa Xóm Nghiện mãi xinh mãi nghiện và đừng lấy chồng sớm ',
     btnAccept: 'Okiiiii lun. <3',
     messLink: 'https://www.facebook.com/messages/t/3777985598941448',
 }
